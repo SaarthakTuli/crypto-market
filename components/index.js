0163fed0750f3e7a253a6bad22a6944aab5bafe6
SaarthakTuli <saarthaktuli@gmail.com>
2022-11-30 20:16:51 +0530

    layout

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import HomeHeader from './HomeHeader';
+import { CircleButton, RectButton } from './Button';
+
+export {
+    FocusedStatusBar,
+    NFTCard,
+    HomeHeader,
+    CircleButton,
+    RectButton
+}
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native"
+import { useState } from "react"
+import { View, Text, SafeAreaView, FlatList } from "react-native"
+
+import {COLORS, NFTData} from '../constants'
+
+import {HomeHeader, NFTCard, FocusedStatusBar} from '../components';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home page..</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <FocusedStatusBar background={COLORS.primary} />
 
-    </View>
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({item}) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        {/* Background Colors... */}      
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}>
+
+          
+          <View style={{height: 300, backgroundColor: COLORS.primary}} />
+
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+
+        </View>
+
+      </View>
+
+    </SafeAreaView>
   )
 }
 
