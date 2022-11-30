c0f2a3599a236eda05c503ebc81f032dd2c655ef
SaarthakTuli <saarthaktuli@gmail.com>
2022-11-30 16:31:55 +0530

    setup

---
+++ screens/Home.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home page..</Text>
+
+    </View>
+  )
+}
+
+export default Home
---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import { default as Home } from './Home'
+import { default as Details } from './Details'
+
+export {
+    Home,
+    Details
+}
